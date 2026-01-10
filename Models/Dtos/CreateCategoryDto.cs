c6a42c7bee0e357b0fb30c0fc21db3fbdee90064
Jesus Sanchez <jesus.sr0704@gmail.com>
2026-01-09 22:27:51 -0600

    Added Category Dtos

---
+++ Models/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace webApi.Models.Dtos;
+
+public class CreateCategoryDto
+{
+    [Required(ErrorMessage = "Name is required")]
+    [MaxLength(50, ErrorMessage = "Name can't exceed 50 characters")]
+    [MinLength(3, ErrorMessage = "Name must be at least 3 characters")]
+
+    public string Name { get; set; } = string.Empty;
+}
